ae5b28c7afd6d7fcf30f3d8051648d80364af15b
ameya-g-git <121361927+ameya-g-git@users.noreply.github.com>
2024-02-24 18:23:04 -0500

    changed output path name

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 input_file = 'billy_720'
 input_path = f'billies/{input_file}'
-output_path = f'./{input_file}/{input_file}_compressed'
+output_path = f'./{input_file}/{input_file}_quantized'
 csv_file = f'./{input_file}/{input_file}.csv'
 iterations = 8
 trials = 7
